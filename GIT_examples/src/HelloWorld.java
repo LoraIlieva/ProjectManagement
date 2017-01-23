56312c2e9d2a6ac7d5bee908c5f41435f3d2e372
student <student@uni>
2017-01-23 21:39:59 +0200

    asdf

--- GIT_examples/src/HelloWorld.java
+++ GIT_examples/src/HelloWorld.java
@@ -4,7 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int b = 5;
-		System.out.println(b+1);
+		System.out.println(b+1);	System.out.println(b+12);
 	}
-
 }
