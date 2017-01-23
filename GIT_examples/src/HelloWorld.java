62665cf662ecde220dc4f8d903cfe81e6de08000
student <student@uni>
2017-01-23 21:38:12 +0200

    sum

--- GIT_examples/src/HelloWorld.java
+++ GIT_examples/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int b = 5;
-		System.out.println(b);
+		System.out.println(b+1);
 	}
 
 }
