17a1eed4d30a42faaf32fb712f1209f26ac0f754
student <student@uni>
2017-01-23 21:28:39 +0200

    second message - conlifct

--- GIT_examples/src/HelloWorld.java
+++ GIT_examples/src/HelloWorld.java
@@ -3,8 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 5;
-		System.out.println(a);
+		int b = 5;
+		System.out.println(b);
 	}
 
 }
